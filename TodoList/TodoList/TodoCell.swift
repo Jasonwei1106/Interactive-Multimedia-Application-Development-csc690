77ce017115ea4efb1156741d37a3674067ab1fc6
Thomas Yu <yyu10@mail.sfsu.edu>
2020-04-19 23:53:43 -0700

    UPDATE

--- TodoList/TodoList/TodoCell.swift
+++ TodoList/TodoList/TodoCell.swift
@@ -8,23 +8,8 @@
 
 import UIKit
 
-protocol Change {
-    func change(check: Bool) ->Bool
-}
-
 class TodoCell: UITableViewCell {
 
-    @IBAction func Check(_ sender: Any) {
-        let temp = delegate!.change(check: lists![index!].check)
-        print(temp)
-        lists![index!].check = temp
-    }
-    @IBOutlet weak var Checkbox: UIButton!
-    
+  
     @IBOutlet weak var labelName: UILabel!
-    
-    var delegate: Change?
-    var lists: [todo]?
-    var index : Int?
-
 }
